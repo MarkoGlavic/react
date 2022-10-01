a9db797f0756fc074361b5b574faa186b3cea22c
Marko Glavic <glavim@gmail.com>
2022-10-01 19:59:13 +0100

    done!

--- src/components/exercises/04_iteration.js
+++ src/components/exercises/04_iteration.js
@@ -2,9 +2,27 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = (props) => {
+  const list = props.modules.map((f, index) => 
+  <tr key={index}>
+    <td>{props.modules[index].name}</td>
+    <td>{props.modules[index].noLectures}</td>
+    <td>{props.modules[index].noPracticals}</td>
+  </tr>);
   return (
-      <h1>TODO</h1>
+    <>
+      <h1>{props.course}</h1>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No Practicals</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+      </>
   );
 };
 
